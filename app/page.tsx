5b988b3084e1e3c167b3d920e4fa7bef0ecc824a
Erlliam Mejia <tommejas@gmail.com>
2025-02-24 19:11:00 -0600

    Minor grid/item styling

--- app/page.tsx
+++ app/page.tsx
@@ -27,17 +27,32 @@ export default function Home() {
 }
 
 function UploadedFiles({ files }: { files: FileInfo[] }) {
+  // ChatGPT
+  function formatBytes(bytes: number, decimals = 2) {
+    if (bytes === 0) return "0 Bytes";
+
+    const k = 1024; // Size of 1 KB
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+  }
+
   return (
     <>
       {files.length === 0 ? (
         <p>No uploaded files found. Upload to get started</p>
       ) : (
-        <ul>
+        <ul className="flex flex-wrap gap-4">
           {files.map((file, index) => (
-            <li key={index}>
-              <p>{file.name}</p>
-              <p>Uploaded: {file.uploadDate}</p>
-              <p>Size: {file.size} bytes</p>
+            <li key={index} className="border border-red-100 p-4">
+              <div>
+                <p>{file.name}</p>
+                <p>{new Date(file.uploadDate).toLocaleString()}</p>
+                <p>{formatBytes(file.size)}</p>
+              </div>
 
               {file.isImage && (
                 <Image
@@ -53,6 +68,14 @@ function UploadedFiles({ files }: { files: FileInfo[] }) {
                   <source src={file.url} />
                 </video>
               )}
+
+              <div className="flex gap-4">
+                <button>Copy</button>
+                <a href={file.url} download>
+                  Download
+                </a>
+                <button>Delete</button>
+              </div>
             </li>
           ))}
         </ul>
